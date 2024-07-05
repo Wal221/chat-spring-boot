9743c544062581a588c581759f634ee43c1fb6ce
Walmircsanto <walmirsantosrosa@gmail.com>
2024-07-05 10:46:51 -0300

    nome da classe alterado

--- src/main/java/br/com/chat/ChatSpringBootApplication.java
+++ src/main/java/br/com/chat/ChatSpringBootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatSpringBootApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChatSpringBootApplication.class, args);
 	}
